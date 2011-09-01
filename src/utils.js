b372365f265bc775278d0268ea684bf75239a181
David Hrachový <davidhrachovy@atlas.cz>
2011-09-01 23:07:38 +0200

    rotating square

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,31 @@
+var mvMatrixStack = [];
+
+function mvPushMatrix(m) {
+  if (m) {
+    mvMatrixStack.push(M4x4.clone(m));
+    mvMatrix = M4x4.clone(m);
+  } else {
+    mvMatrixStack.push(M4x4.clone(mvMatrix));
+  }
+}
+
+function mvPopMatrix() {
+  if (!mvMatrixStack.length) {
+    throw("Can't pop from an empty matrix stack.");
+  }
+  
+  mvMatrix = mvMatrixStack.pop();
+  return mvMatrix;
+}
+
+function mvRotate(angle, axis) {
+  var inRadians = angle * Math.PI / 180.0;
+  
+  M4x4.rotate(inRadians, axis, mvMatrix, mvMatrix);
+}
+
 function loadIdentity() {
-  mvMatrix = M4x4.I;
+  mvMatrix = M4x4.clone(M4x4.identity);
 }
 
 function multMatrix(m) {
@@ -7,7 +33,7 @@ function multMatrix(m) {
 }
 
 function mvTranslate(v) {
-  mvMatrix = M4x4.makeTranslate(v);
+  M4x4.translate(v, mvMatrix, mvMatrix);
 }
 
 function setMatrixUniforms() {
@@ -17,3 +43,13 @@ function setMatrixUniforms() {
   var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
   gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix));
 }
+requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+     window.webkitRequestAnimationFrame ||
+     window.mozRequestAnimationFrame ||
+     window.oRequestAnimationFrame ||
+     window.msRequestAnimationFrame ||
+     function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+       window.setTimeout(callback, 1000/60);
+     };
+})();
